27e9d32565fb3e0d696a4e2df0f0f0ad008843e4
Jatin Yadav <jatin.yadav@favouritetable.com>
2023-10-06 17:32:23 +0100

    Finished Boolean operators

--- Lesson1.py
+++ Lesson1.py
@@ -1,6 +1,5 @@
 ### Variable assignment ###
 
-
 ##  We can assign a value of any data type to a variable
 ##  (and give it any name we want)
 ##  The values stored in these variables can be changed at any time during a program
@@ -31,7 +30,6 @@ list_of_string = ["abcd", "efgh", "ijkl"]
 
 ### Constants ###
 
-
 ##  Constants are like variables except that their values 
 ##  cannot be changed while the program is running
 
@@ -39,10 +37,9 @@ list_of_string = ["abcd", "efgh", "ijkl"]
 
 ### Operators ###
 
+##  Mathematical operators
 
-##  Mathematical operatos
-
-#   Feel free to change these and see the outcomes
+#   Feel free to change these and see the outcomes (just don't let num2 be 0)
 num1 = 10
 num2 = 4
 
@@ -89,9 +86,71 @@ print(result)
 print()
 
 
-## Boolean Operators
+##  Boolean Operators
+##  These all return a boolean value
 
-#  
+#   Comparitive operators
+#   Less than <, greater than >
+result = num1 > num2
+print("num1 > num2")
+print(result)
+print()
+
+result = num1 < num2
+print("num1 < num2")
+print(result)
+print()
+
+#   Less/greater than or equal to
+result = num1 >= num2
+print("num1 >= num2")
+print(result)
+print()
+
+result = num1 <+ num2
+print("num1 <= num2")
+print(result)
+print()
+
+#   Equal to ==
+result = num1 == num2
+print("num1 == num2")
+print(result)
+print()
+
+
+##  Logical Operators
+
+a = True
+b = False
+
+#   AND
+#   If we have input a and b (T - true, F - false)
+#    a | b | a AND b 
+#   ---+---+---------
+#    F | F |    F
+#    F | T |    F
+#    T | F |    F
+#    T | T |    T
+
+result = a and b
+print("a and b")
+print(result)
+print()
+
+#   OR
+#   If we have input a and b (T - true, F - false)
+#    a | b | a OR b 
+#   ---+---+--------
+#    F | F |   F
+#    F | T |   T
+#    T | F |   T
+#    T | T |   T
+
+result = a or b
+print("a or b")
+print(result)
+print()
 
 
 ### Strings ###
